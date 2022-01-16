77006bc211aea5047ab9fc6b6e942d39b87f1cab
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-01-16 22:36:48 +0000

    Updated the API integration format and changed the HTML in App.js and WeatherSearch.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="container">
         <div className="app-wrapper">
           <header className="App-header">
-            <WeatherSearch defaultCity="Madrid" />
+            <WeatherSearch defaultCity="Arecibo" />
             <Footer />
           </header>
         </div>
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -3,38 +3,22 @@ import axios from "axios";
 import "./WeatherSearch.css";
 
 export default function WeatherSearch(props) {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
-  const [description, setDescription] = useState(null);
-  const [humidity, setHumidity] = useState(null);
-  const [wind, setWind] = useState(null);
-  const [city, setCity] = useState(null);
-  const [icon, setIcon] = useState(null);
-
-  function handleSearch(event) {
-    event.preventDefault();
-    let apiKey = "d6648610e3c1c3aed8194b8aaf46b519";
-    let units = "metric";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(url).then(getForecast);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      date: "Thursday 7:33",
+    });
   }
 
-  function getForecast(response) {
-    setReady(true);
-    setTemperature(Math.round(response.data.main.temp));
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWind(Math.round(response.data.wind.speed));
-    setIcon(
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-  }
-
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
-
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div class="container">
         <div class="card">
@@ -42,11 +26,10 @@ export default function WeatherSearch(props) {
             <div class="search-city">
               <nav class="navbar navbar-light bg-light">
                 <div class="container-fluid">
-                  <form class="d-flex" id="search-form" onSubmit={handleSearch}>
+                  <form class="d-flex" id="search-form">
                     <input
                       className="form-control me-2"
                       type="search"
-                      onChange={changeCity}
                       placeholder="Enter a city..."
                       aria-label="Search"
                       id="city-text-input"
@@ -65,49 +48,45 @@ export default function WeatherSearch(props) {
               </nav>
             </div>
           </div>
-          <br />
-          <div className="container px-4 weather-today">
-            <div className="row">
-              <div className="col">
-                <div className="city-name">
-                  <br />
-                  <h1>{city}</h1>
-                </div>
-              </div>
-              <div className="col">
+        </div>
+
+        <div class="row row-cols-1 row-cols-md-2 g-4 mt-2">
+          <div class="col">
+            <div class="card ">
+              <div class="card-body">
+                <h1 className="city mb-4 me-2">{weatherData.city}</h1>
+
                 <div className="temp">
-                  <span className="main-temp">{temperature}</span>
-                  <span className="degrees">℃ | ℉</span>{" "}
+                  <span className="main-temp me-2">
+                    {Math.round(weatherData.temperature)}
+                  </span>
+                  <span className="degrees">℃ | ℉</span>
                 </div>
               </div>
             </div>
           </div>
-          <div className="container px-4 weather-today">
-            <div className="row">
-              <div className="col">
-                <div className="weather-icon">
-                  <img src={icon} alt="weather icon" />
-                </div>
-                <div className="description text-capitalize">{description}</div>
-                <br />
-              </div>
 
-              <div className="col">
-                <div className="stats">
-                  <ul>
-                    <li className="weekday text-muted">Thursday</li>
-                    <li className="time fw-bold">19:33</li>
-                  </ul>
-                  <ul>
-                    <li>
-                      <strong>Humidity:</strong> {humidity}%
-                    </li>
-                    <li>
-                      <strong>Wind:</strong>
-                      {wind}km/h
-                    </li>
-                  </ul>
+          <div class="col">
+            <div class="card ms-5 mb-2">
+              <div class="card-body">
+                <div className="description text-capitalize mb-2 ms-4">
+                  {weatherData.description}
                 </div>
+                <div className="weather-icon mb-2 ms-5">
+                  <img src={weatherData.iconUrl} alt="" />
+                </div>
+              </div>
+              <div className="stats">
+                <ul>
+                  <li className="now text-muted ">{weatherData.date}</li>
+                  <li>
+                    <strong>Humidity:</strong> {weatherData.humidity}%
+                  </li>
+                  <li>
+                    <strong>Wind:</strong>
+                    {Math.round(weatherData.wind)}km/h
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
@@ -115,88 +94,11 @@ export default function WeatherSearch(props) {
       </div>
     );
   } else {
-    let apiKey = "d6648610e3c1c3aed8194b8aaf46b519";
+    const apiKey = "d6648610e3c1c3aed8194b8aaf46b519";
     let units = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(getForecast);
-    return (
-      <div class="container">
-        <div class="card">
-          <div class="card-header">
-            <div class="search-city">
-              <nav class="navbar navbar-light bg-light">
-                <div class="container-fluid">
-                  <form class="d-flex" id="search-form" onSubmit={handleSearch}>
-                    <input
-                      className="form-control me-2"
-                      type="search"
-                      onChange={changeCity}
-                      placeholder="Enter a city..."
-                      aria-label="Search"
-                      id="city-text-input"
-                      autocomplete="off"
-                    />
-                    <button
-                      className="btn btn-outline-info me-2"
-                      type="Submit"
-                      defaultValue="Search"
-                      id="search-city"
-                    >
-                      Search
-                    </button>
-                  </form>
-                </div>
-              </nav>
-            </div>
-          </div>
-          <br />
-          <div className="container px-4 weather-today">
-            <div className="row">
-              <div className="col">
-                <div className="city-name">
-                  <br />
-                  <h1>{props.defaultCity}</h1>
-                </div>
-              </div>
-              <div className="col">
-                <div className="temp">
-                  <span className="main-temp">{temperature}</span>
-                  <span className="degrees">℃ | ℉</span>{" "}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container px-4 weather-today">
-            <div className="row">
-              <div className="col">
-                <div className="weather-icon">
-                  <img src={icon} alt="weather icon" />
-                </div>
-                <div className="description text-capitalize">{description}</div>
-                <br />
-              </div>
+    axios.get(apiUrl).then(handleResponse);
 
-              <div className="col">
-                <div className="stats">
-                  <ul>
-                    <li className="weekday text-muted">Thursday</li>
-                    <li className="time fw-bold">19:33</li>
-                  </ul>
-                  <ul>
-                    <li>
-                      <strong>Humidity:</strong> {humidity}%
-                    </li>
-                    <li>
-                      <strong>Wind:</strong>
-                      {wind}km/h
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return "Loading...";
   }
 }
