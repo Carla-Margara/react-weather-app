9de213ed717436daf630a75bed0d4b2adc6917d9
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-01-29 08:43:17 +0000

    Added five-day forecast in WeeklyForecast.js and updated WeeklyForecast.css

--- src/WeeklyForecast.js
+++ src/WeeklyForecast.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeeklyForecast.css";
 
 export default function WeeklyForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -16,9 +20,15 @@ export default function WeeklyForecast(props) {
     return (
       <div className="WeeklyForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
